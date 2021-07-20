0a677caa47df0821b9dddacd0d524db8887b2ddb
Tom Smith <tomossmith86@icloud.com>
2021-07-20 21:16:38 +0000

    Created Javascript functions to change cheese images upon dropdown selection

--- assets/js/build-pizza.js
+++ assets/js/build-pizza.js
@@ -10,3 +10,15 @@ $( document ).ready(function sauceDropdownFunction() {
         $("#show" + ingredientVariable).show();
     });
 });
+
+// CREDIT: https://tutorialdeep.com/knowhow/change-image-on-dropdown-select-option-jquery/
+
+// Cheese Dropdown Function  
+
+$(document).ready(function cheeseDropdownFunction(){
+    $('#cheeseType').on('change', function(){
+       var ingredientVariable = $(this).val(); 
+        $("div.cheeseImages").hide();
+        $("#show"+ingredientVariable).show();
+    });
+ });
