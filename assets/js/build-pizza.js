9f3c03323140c5ed3d54ed65ba52957002587f67
Tom Smith <tomossmith86@icloud.com>
2021-07-21 21:32:05 +0000

    Built the code to calculate the total cost of the pizza depending on the choices made

--- assets/js/build-pizza.js
+++ assets/js/build-pizza.js
@@ -38,47 +38,63 @@ $(document).ready(function cheeseDropdownFunction(){
 
   // PRICE CALCULATOR FUNCTIONS
   // Global Price Function Variables
-var cheeseSubtotal = 0;
-var sauceSubtotal = 0;
+var cheeseSubtotal = 0.00;
+var sauceSubtotal = 0.00;
 
 // Sauce Subtotal
 $('#sauceType').change(function() {
-    var sauceSubtotal = 0;
+    var sauceSubtotal = 0.00;
     $('#sauceType').each(function() {
         if($(this).val() == 'Mariana') {
-            sauceSubtotal += parseFloat(0.99);
+            sauceSubtotal += parseFloat(1.00);
         }
         else if($(this).val() == 'BBQ') {
-          sauceSubtotal += parseFloat(0.99);
+          sauceSubtotal += parseFloat(1.10);
       }
       else {
        sauceSubtotal += parseFloat(0.00)
       }
     });
-    //console.log(cheeseSubtotal)
-    $('#sauceSubtotal').text('£' + sauceSubtotal.toFixed(2));
+    document.getElementById("sauceSubtotal").innerHTML = sauceSubtotal.toFixed(2);
  });
 
  // Cheese Subtotal
  $('.cheese').change(function() {
-    var cheeseSubtotal = 0;
+    var cheeseSubtotal = 0.00;
     $('.cheese').each(function() {
-        if($(this).val() == 'standardCheese') {
-            cheeseSubtotal += parseFloat(0.99);
+        if($(this).val() == 'Add Cheese?') {
+            cheeseSubtotal += parseFloat(0.00);
+        }
+        else if($(this).val() == 'standardCheese') {
+            cheeseSubtotal += parseFloat(1.00);
         }
         else if($(this).val() == 'extraCheese') {
-          cheeseSubtotal += parseFloat(1.99);
+          cheeseSubtotal += parseFloat(2.00);
       }
       else {
        cheeseSubtotal += parseFloat(0.00)
       }
     });
-    //console.log(cheeseSubtotal)
-    $('#cheeseSubtotal').text('£' + cheeseSubtotal.toFixed(2));
+    document.getElementById("cheeseSubtotal").innerHTML = cheeseSubtotal.toFixed(2);
 });
 
 // Topping Subtotal
 function calculateToppingCost() {
     var numberOfCheckedToppings = $('input:checkbox:checked').length;
     var toppingSubtotal = numberOfCheckedToppings * 0.50
-    $('#toppingSubtotal').text('£' + toppingSubtotal.toFixed(2));}
+    document.getElementById("toppingSubtotal").innerHTML = toppingSubtotal.toFixed(2);
+}
+
+
+function pizzaTotal() {
+val_1 = Number($('#toppingSubtotal').html()), 
+val_2 = Number($('#cheeseSubtotal').html()),
+val_3 = Number($('#sauceSubtotal').html());
+
+var total = 1 + val_1 + val_2 + val_3;
+console.log(val_1);
+console.log(val_2);
+console.log(val_3);
+console.log(total);
+document.getElementById("pizzaTotalCost").innerHTML = total.toFixed(2);
+}
