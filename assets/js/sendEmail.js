f81ae31a00fab4e12a62b77cc9e4c3e5178d1bd8
Tom Smith <tomossmith86@icloud.com>
2021-07-27 19:13:25 +0000

    added a function to send the pizza cost to emailjs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,6 +1,55 @@
 function sendMail(pizzaForm) {
     console.log("EmailJS Loaded")
 
+
+        var checkPepperoni = "No";
+        var checkChillies = "No";
+        var checkHam = "No";
+        var checkMushrooms = "No";
+        var checkPeppers = "No";
+        var checkPineapple = "No";
+        var checkPrawns = "No";
+        var checkHerbs = "No";
+        var checkOlives = "No";
+        var checkOnions = "No";
+        var checkTomatoes = "No";
+
+        if (document.getElementById("Pepperoni").checked) {
+          checkPepperoni = "Yes";
+        }
+        if (document.getElementById("Chillies").checked) {
+            checkChillies = "Yes";
+        }
+          if (document.getElementById("Ham").checked) {
+            checkHam = "Yes";
+        }
+        if (document.getElementById("Mushrooms").checked) {
+            checkMushrooms = "Yes";
+          }
+          if (document.getElementById("Peppers").checked) {
+            checkPeppers = "Yes";
+          }
+          if (document.getElementById("Pineapple").checked) {
+            checkPineapple = "Yes";
+          }
+          if (document.getElementById("Prawns").checked) {
+            checkPrawns = "Yes";
+          }
+          if (document.getElementById("Herbs").checked) {
+            checkHerbs = "Yes";
+          }
+          if (document.getElementById("Olives").checked) {
+            checkOlives = "Yes";
+          }
+          if (document.getElementById("Onions").checked) {
+            checkOnions = "Yes";
+          }
+          if (document.getElementById("Tomatoes").checked) {
+            checkTomatoes = "Yes";
+          }
+
+
+
     // Pass the checkbox name to the function
 function getCheckedBoxes(pizza_topping) {
     var checkboxes = document.getElementsByName(pizza_topping);
@@ -17,7 +66,74 @@ function getCheckedBoxes(pizza_topping) {
   }
   
   // Call as
-  var checkedBoxes = getCheckedBoxes("mycheckboxes");
+  var checkedBoxes = getCheckedBoxes("pizza_topping");
+
+
+  // Pricing
+
+    // PRICE CALCULATOR FUNCTIONS
+  // Global Price Function Variables
+var cheeseSubtotal = 0.00;
+var sauceSubtotal = 0.00;
+
+// Sauce Subtotal
+$('#sauceType').change(function() {
+    var sauceSubtotal = 0.00;
+    $('#sauceType').each(function() {
+        if($(this).val() == 'Mariana') {
+            sauceSubtotal += parseFloat(1.00);
+        }
+        else if($(this).val() == 'BBQ') {
+          sauceSubtotal += parseFloat(1.10);
+      }
+      else {
+       sauceSubtotal += parseFloat(0.00)
+      }
+    });
+
+ });
+
+ // Cheese Subtotal
+ $('.cheese').change(function() {
+    var cheeseSubtotal = 0.00;
+    $('.cheese').each(function() {
+        if($(this).val() == 'Add Cheese?') {
+            cheeseSubtotal += parseFloat(0.00);
+        }
+        else if($(this).val() == 'standardCheese') {
+            cheeseSubtotal += parseFloat(1.00);
+        }
+        else if($(this).val() == 'extraCheese') {
+          cheeseSubtotal += parseFloat(2.00);
+      }
+      else {
+       cheeseSubtotal += parseFloat(0.00)
+      }
+    });
+});
+
+// Topping Subtotal
+function calculateToppingCost() {
+    var numberOfCheckedToppings = $('input:checkbox:checked').length;
+    var toppingSubtotal = numberOfCheckedToppings * 0.50;
+};
+
+//Pizza Total Cost
+
+val_1 = Number($('#sizeSubtotal').html()),
+val_2 = Number($('#crustSubtotal').html()),
+val_3 = Number($('#toppingSubtotal').html()), 
+val_4 = Number($('#cheeseSubtotal').html()),
+val_5 = Number($('#sauceSubtotal').html());
+
+
+var subtotal = 1 + val_1 + val_2 + val_3 + val_4 + val_5;
+var total = subtotal.toFixed(Math.max(((subtotal+'').split(".")[1]||"").length, 2));
+
+console.log(subtotal);
+console.log(total);
+
+// Send Email
 
     emailjs.send("service_be3wq2q", "mario", {
         "from_name": pizzaForm.name.value,
@@ -28,25 +144,30 @@ function getCheckedBoxes(pizza_topping) {
         "size": pizzaForm.size.value,
         "base": pizzaForm.crust.value,
         "sauce": pizzaForm.sauceType.value,
-        "pepperoni": pizzaForm.topping_1.value,
-        "chillies": pizzaForm.topping_2.value,
-        "ham": pizzaForm.topping_3.value,
-        "mushrooms": pizzaForm.topping_4.value,
-        "peppers": pizzaForm.topping_5.value,
-        "pineapple": pizzaForm.topping_6.value, 
-        "prawns": pizzaForm.topping_7.value, 
-        "italian_herbs": pizzaForm.topping_8.value, 
-        "olives": pizzaForm.topping_9.value, 
-        "onions": pizzaForm.topping_10.value, 
-        "tomatoes": pizzaForm.topping_11.value,
+        "pepperoni": checkPepperoni,
+        "chillies": checkChillies,
+        "ham": checkHam,
+        "mushrooms": checkMushrooms,
+        "peppers": checkPeppers,
+        "pineapple": checkPineapple,
+        "prawns": checkPrawns,
+        "italian-herbs": checkHerbs,
+        "olives": checkOlives,
+        "onions": checkOnions,
+        "tomatoes": checkTomatoes,
+        "price": total,
+
+
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            alert("Thank You For Your Order, You will shortly receive an email confirmation.")
         },
         function(error) {
             console.log("FAILED", error);
-        }
-    );
+            alert("Something went wrong, please check you have completed all the fields correctly and try again.")
+        })
+
     return false;  // To block from loading a new page
 }
